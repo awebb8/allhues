acde22422b1b064b0d8351330b03c6e4ae3fd453
Andrew Siegel <siegelal7@gmail.com>
2020-11-07 00:56:05 -0500

    got login backend built

--- routes/api/contentCreators.js
+++ routes/api/contentCreators.js
@@ -52,42 +52,38 @@ router.post("/register", async (req, res) => {
     res.status(400).json({ error: e.message });
   }
 });
-// router.post("/", (req, res) => {
-//   const { name, email, password } = req.body;
 
-//   // Validation
-//   if (!name || !email || !password) {
-//     return res.status(400).json({ msg: "Please enter all fields" });
-//   }
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-//   // Check for existing user
-//   contentCreator.findOne({ email }).then((user) => {
-//     if (user) {
-//       return res.status(400).json({ msg: "User already exists" });
-//     }
-//     const newcontentCreator = new contentCreator({
-//       name,
-//       email,
-//       password,
-//     });
+  // Simple validation
+  if (!email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
 
-//     // Create salt & hash
-//     bcrypt.genSalt(10, (err, salt) => {
-//       bcrypt.hash(newcontentCreator.password, salt, (err, hash) => {
-//         if (err) throw err;
-//         newcontentCreator.password = hash;
-//         newcontentCreator.save().then((user) => {
-//           res.json({
-//             user: {
-//               id: user.id,
-//               name: user.name,
-//               email: user.email,
-//             },
-//           });
-//         });
-//       });
-//     });
-//   });
-// });
+  try {
+    // Check for existing user
+    const user = await contentCreator.findOne({ email });
+    if (!user) throw Error("User does not exist");
 
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw Error("Invalid credentials");
+
+    const token = jwt.sign({ id: user._id }, "sneakysecret", {
+      expiresIn: 3600,
+    });
+    if (!token) throw Error("Couldnt sign the token");
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
+});
 module.exports = router;
