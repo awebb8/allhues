54ecc1ba7d99efb2b8071a512c3c63faec53bbb9
Alkawe <alixkwebb@gmail.com>
2020-11-10 13:02:09 -0500

    added code to show a kit card that can be modified

---
+++ client/src/components/UpdateKit/UpdateKit.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const UpdateKit = (props) => {
+    return (
+        <div className="card">
+          <img
+            src={
+              props.src === undefined ? "http://via.placeholder.com/200" : props.src
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title" style={{ textAlign: "center" }}>
+              <input type="text" name="kitName" onChange={props.handleInputChange} defaultValue={props.info.kitName ? props.info.kitName : ""}></input>
+            </h5>
+            <p className="card-text" style={{ textAlign: "center" }}>
+            <input type="text" name="kitDescription" onChange={props.handleInputChange} defaultValue={props.info.kitDescription ? props.info.kitDescription : ""}></input>
+            <br/>
+            <button className="buttons" onClick={props.onClickUpdate}>Save</button>
+            </p>
+            {/* <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p> */}
+          </div>
+        </div>
+    );
+};
+
+export default UpdateKit;
