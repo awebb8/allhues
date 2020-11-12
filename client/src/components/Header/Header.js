9b1dbb09620d21d352670b64035f7f687049cea9
Alkawe <alixkwebb@gmail.com>
2020-11-11 22:12:13 -0500

    modified styling

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./header.css"
 function Header() {
     return(
         <>
-        <div className="main-image jumbotron">
+        <div className="main-image jumbotron-fluid">
             <h1 className="display-4 title">AllHues</h1>
             {/* <p className="lead">Explore and celebrate the beautiful hue that you are.</p> */}
             <p className="lead">A million shades of beautiful</p>
