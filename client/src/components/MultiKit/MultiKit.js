8508efc1bb9dfec5e4e3bbb117035c11c2c53f92
Alkawe <alixkwebb@gmail.com>
2020-11-12 22:20:00 -0500

    added favorites button and ability to toggle

--- client/src/components/MultiKit/MultiKit.js
+++ client/src/components/MultiKit/MultiKit.js
@@ -1,13 +1,24 @@
-import React from "react";
 import "./MultiKit.css";
 import { useHistory } from "react-router-dom";
+import React, { useState } from "react";
 
 const MultiKit = (props) => {
   const history = useHistory();
+
+  const [favorite, setFavorite] = useState(false);
+
   const handleSingleKitClick = (e) => {
     console.log(e.target);
     history.push(`/viewone/${e.target.getAttribute("class").substr(0, 25)}`);
   };
+
+  const handleFavoritesClick = (e) => {
+    e.stopPropagation();
+    console.log(e.target);
+    setFavorite(!favorite);
+    console.log(favorite);
+  };
+
   return (
     <div
       className="grow col mb-4"
@@ -19,9 +30,7 @@ const MultiKit = (props) => {
       onClick={(e) => handleSingleKitClick(e)}
       style={{ cursor: "pointer" }}
     >
-      <div
-        className={props.class === undefined ? "card" : props.class + " card"}
-      >
+      <div className={props.class === undefined ? "card" : props.class + " card"}>
         <img
           src={
             props.src === undefined
@@ -37,6 +46,9 @@ const MultiKit = (props) => {
             props.class === undefined ? "card-body" : props.class + " card-body"
           }
         >
+          <button type="button" className="btn btn-default" style={{backgroundColor: favorite ? "pink" : "white"}} 
+            onClick={(e) => handleFavoritesClick(e)}>
+            <i className="far fa-heart"></i></button>
           <h5
             style={{ textAlign: "center" }}
             className={
