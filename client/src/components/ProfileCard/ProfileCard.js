f1875dea419018fdaf1bbb077c10d385b713ea56
Developer Raj <rajdeepkakar@gmail.com>
2020-11-13 17:13:08 -0500

    Updated the contentCreatorPortal page with profile cards and added two metrics. Total kit views and Total number of kits created. Fixed image upload issue.

--- client/src/components/ProfileCard/ProfileCard.js
+++ client/src/components/ProfileCard/ProfileCard.js
@@ -1,119 +1,134 @@
 import React, { useContext, useEffect, useState } from "react";
-
+import profilecard from "./profilecard.css";
 import NameContext from "../../utils/NameContext";
 import RoleContext from "../../utils/RoleContext";
 import UserContext from "../../utils/UserContext";
 import axios from "axios";
 import API from "../../utils/API";
 
-const ProfileCard = () => {
-  const [image, setImage] = useState("");
-  const [usersName, setUsersName] = useState("");
-  // const { name } = useContext(NameContext);
-  const { role } = useContext(RoleContext);
-  const { id } = useContext(UserContext);
+const ProfileCard = (props) => {
+	let totalKitViews = 0;
+  props.yourKits.forEach(kit => (
+    totalKitViews += kit.uniqueVisits
+  ))
+	const [uploadedImage, setUploadedImage] = useState("");
+	const [image, setImage] = useState("");
+	const [usersName, setUsersName] = useState("");
+	// const { name } = useContext(NameContext);
+	const { role } = useContext(RoleContext);
+	const { id } = useContext(UserContext);
 
-  const onChange = (e) => {
-    setImage(e.target.files[0]);
-  };
-  const url = "https://api.cloudinary.com/v1_1/dsi7lpcmx/image/upload";
-  const preset = "askckkso";
+	const onChange = (e) => {
+		setUploadedImage(e.target.files[0]);
+		//onSubmit(e);
+	};
+	const url = "https://api.cloudinary.com/v1_1/dsi7lpcmx/image/upload";
+	const preset = "askckkso";
 
-  useEffect(() => {
-    API.getUser().then((res) => {
-      setImage(res.data.image);
-      setUsersName(res.data.name);
-      console.log(res.data.image);
-    });
-  }, []);
+	useEffect(() => {
+		if (uploadedImage) {
+			onSubmit();
+		}
+	}, [uploadedImage]);
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
+	useEffect(() => {
+		API.getUser().then((res) => {
+			setImage(res.data.image);
+			//setUploadedImage(res.data.image);
+			setUsersName(res.data.name);
+			console.log(res.data.image);
+		});
+	}, []);
 
-    const formData = new FormData();
-    formData.append("file", image);
-    formData.append("upload_preset", preset);
-    try {
-      // setLoading(true);
-      // TODO: Note that we deleted the custom header cuz 3rd party api
-      // delete axios.defaults.headers["x-auth-token"];
-      const res = await axios.post(url, formData);
-      const imageUrl = res.data.secure_url;
-      console.log(imageUrl);
+	const onSubmit = async () => {
+		const formData = new FormData();
+		formData.append("file", uploadedImage);
+		formData.append("upload_preset", preset);
+		try {
+			// setLoading(true);
+			// TODO: Note that we deleted the custom header cuz 3rd party api
+			// delete axios.defaults.headers["x-auth-token"];
+			const res = await axios.post(url, formData);
+			const imageUrl = res.data.secure_url;
+			console.log(imageUrl);
 
-      setImage(res.data.secure_url);
-      axios
-        .put(`/api/users/${id}`, { image: res.data.secure_url })
-        .then((res) => {
-          console.log(res.data);
-        });
-    } catch (err) {
-      console.error(err);
-    }
+			setImage(res.data.secure_url);
+			axios
+				.put(`/api/users/${id}`, { image: res.data.secure_url })
+				.then((res) => {
+					console.log(res.data);
+				});
+		} catch (err) {
+			console.error(err);
+		}
 
-    // Redirect to contentCreator portal
-    // setTimeout(function () {
-    //   history.push("/portal");
-    // }, 1000);
-  };
+		// Redirect to contentCreator portal
+		// setTimeout(function () {
+		//   history.push("/portal");
+		// }, 1000);
+	};
+	//   var shownRole;
+	const determineRoleToShowConsumer = () => {
+		var shownRole;
+		if (role === "Consumer") {
+			shownRole = "Beautiful makeup artist";
+		} else {
+			shownRole = "Influencer";
+		}
+		return shownRole;
+	};
 
-  const jumboStyle = {
-    // margin: "auto",
-    width: "fit-content",
-    height: "fit-content",
-    borderRadius: "15%",
-    position: "absolute",
-    marginLeft: "1%",
-    marginTop: "1%",
-  };
-  //   var shownRole;
-  const determineRoleToShowConsumer = () => {
-    var shownRole;
-    if (role === "Consumer") {
-      shownRole = "Beautiful makeup artist";
-    } else {
-      shownRole = "Influencer";
-    }
-    return shownRole;
-  };
-
-  return (
-    <>
-      <div className="jumbotron" style={jumboStyle}>
-        <img
-          style={{ borderRadius: "95%", marginTop: "-20%" }}
-          src={image}
-          alt="placeholder image"
-        />
-
-        <h4>{usersName}'s profile</h4>
-
-        <p className="lead">{determineRoleToShowConsumer()}</p>
-        <hr className="my-1" />
-        <p>It uses utility classe</p>
-        <label className="btn btn-secondary">
-          Upload
-          <input
-            type="file"
-            id="kitImageInput"
-            className="custom-file-input"
-            name="image"
-            onChange={onChange}
-            accept="image/*"
-            hidden
-          />
-        </label>
-        <button
-          className="btn btn-secondary btn-sm"
-          type="button"
-          onClick={onSubmit}
-          disabled={!image}
-        >
-          Change
-        </button>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="container-fluid">
+				<div className="col-lg-12">
+					<div className="panel profile-cover">
+						<div className="profile-cover__img">
+							<label htmlFor="AvatarImageInput">
+								<input
+									style={{ display: "none" }}
+									type="file"
+									id="AvatarImageInput"
+									className="custom-file-input"
+									name="image"
+									onChange={onChange}
+									accept="image/*"
+									hidden
+								/>
+								<img src={image} alt="placeholder image" />
+							</label>
+							<h3 className="h3">
+								({determineRoleToShowConsumer()}: {usersName})
+							</h3>
+						</div>
+						<div className="profile-cover__action bg--img" data-overlay="0.3">
+							<button className="btn btn-rounded btn-info">
+								<i className="fa fa-plus"></i>
+								<span>Upload</span>
+							</button>
+							<button className="btn btn-rounded btn-info">
+								<i className="fa fa-comment"></i>
+								<span>Message</span>
+							</button>
+						</div>
+						<div className="profile-cover__info">
+							<ul className="nav">
+								<li>
+									<strong>{props.yourKits.length}</strong>Created Kits
+								</li>
+								<li>
+									<strong>33</strong>Favorite Kits
+								</li>
+								<li>
+									<strong>{totalKitViews}</strong>Total Kit Views
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default ProfileCard;
